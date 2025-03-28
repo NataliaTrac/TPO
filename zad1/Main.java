81bb7e850601b093135505491c085b917938b8bc
Natalia Traczewska <165217683+NataliaTrac@users.noreply.github.com>
2025-03-28 02:23:17 +0100

    Add files via upload

---
+++ zad1/Main.java
@@ -0,0 +1,16 @@
+/**
+ *
+ *  @author Traczewska Natalia S24838
+ *
+ */
+
+package zad1;
+
+
+public class Main {
+  public static void main(String[] args) {
+    String dirName = System.getProperty("user.home")+"/TPO1dir";
+    String resultFileName = "TPO1res.txt";
+    Futil.processDir(dirName, resultFileName);
+  }
+}
